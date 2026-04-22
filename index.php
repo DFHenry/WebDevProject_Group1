386b6462342f5a3d372bedb4da43f5231b1ac4c1
David Henry <david_freeman_henry@hotmail.com>
2026-04-21 20:45:51 -0400

    continued stying of index page

--- index.php
+++ index.php
@@ -7,14 +7,14 @@
 ?>
 
 <?php
-    //get 6 menu menu items
-    $menuQuery = 'SELECT name, id, category, image_href, description, price, date_created FROM menu_items ORDER BY date_created DESC LIMIT 6';
+    //get the latest 6 menu items
+    $latestQuery = 'SELECT name, id, category, image_href, description, price, date_created FROM menu_items ORDER BY date_created DESC LIMIT 6';
 
     //prepare sql statement
-    $menuStmt = $db->query($menuQuery);
+    $latestStmt = $db->query($latestQuery);
 
     //get result and place into an array
-    $menuResult = $menuStmt->fetch_all(MYSQLI_ASSOC);
+    $latestResult = $latestStmt->fetch_all(MYSQLI_ASSOC);
 ?>
 
 <h1 id="mainTitle">Three Dudes Bakery</h1>
@@ -26,11 +26,11 @@
         <p>Let's see what three guys can bake together!</p>
     </div>
 </section>
-
+<br>
 <div id="latestItems">
     <h2>Latest Items</h3>
     <div class="frontItemGrid">
-        <?php foreach($menuResult AS $item) : ?>
+        <?php foreach($latestResult AS $item) : ?>
             <div class="frontMenuItem card border-0 shadow-sm rounded-3">
                 <h3 class="frontItemName"><?php echo $item['name'] ?></h3>
                 <p class="frontItemCat"><?php echo $item['category'] ?></p>
@@ -41,7 +41,10 @@
         <?php endforeach ?>
     </div>
 </div>
-
+<div id="pastries">
+    <h2>Pastries & Croissants</h2>
+    
+</div>
 
 
 <?php require_once('components/footer.php'); ?>
